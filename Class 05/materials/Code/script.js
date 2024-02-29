19e88bbc2da219e2069dcaab6a0b2192ca3a4662
Aida Pirusic <pirusic.aida@nsoft.com>
2024-02-29 17:13:08 +0100

    Add materials for class 05

---
+++ Class 05/materials/Code/script.js
@@ -0,0 +1,13 @@
+const pets = ["Bobby", "Cicko", "Rex", "Kitty", "Bambi"];
+
+const grades = [5, 4, 5, 3, 5, 2];
+
+const employees = [
+  { fName: "Bob", lName: "H", gender: "male", age: 19, salary: 1000 },
+  { fName: "Mel", lName: "B", gender: "female", age: 33, salary: 1200 },
+  { fName: "Jill", lName: "M", gender: "female", age: 15, salary: 1000 },
+  { fName: "Lucky", lName: "J", gender: "female", age: 18, salary: 1000 },
+  { fName: "Strike", lName: "K", gender: "male", age: 16, salary: 2300 },
+  { fName: "Eric", lName: "I", gender: "male", age: 17, salary: 1000 },
+];
+
