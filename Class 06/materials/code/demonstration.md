0647bf7a2df55886ce4f32d95770f3ff70741b97
Aida Pirusic <pirusic.aida@nsoft.com>
2024-03-05 14:04:16 +0100

    Add class 06 materials

---
+++ Class 06/materials/code/demonstration.md
@@ -0,0 +1,16 @@
+# Javascript under the hood 🥥
+
+## Where and how is my JS code running?
+
+
+![javascript under the hood](https://raw.githubusercontent.com/sedc-codecademy/skwd9-04-ajs/main/Samples/javascriptandbrowser.png?raw=true)
+
+
+## Working with the browser
+
+
+
+#### behind the scenes
+
+![behind the scenes](https://raw.githubusercontent.com/sedc-codecademy/skwd9-04-ajs/main/Samples/eventloop.gif?raw=true)
+
---
+++ Class 06/materials/code/script.js
@@ -0,0 +1,58 @@
+console.log("first");
+setTimeout(() => {
+  console.log("this happens after 3 seconds!");
+}, 0);
+console.log("last");
+
+let handle = setInterval(() => {
+  //alert("HAOS!");
+  console.log("HEY");
+}, 2000);
+document.querySelector("button").addEventListener("click", () => {
+  clearInterval(handle);
+});
+
+function first() {
+  setTimeout(() => console.log("First thing"), 1000);
+  console.log("Random thing!");
+}
+function second() {
+  console.log("Second thing!");
+}
+first();
+second();
+
+function firstFunction(callback) {
+  setTimeout(() => {
+    console.log("First thing");
+    callback();
+  }, 1000);
+}
+function secondFunction() {
+  console.log("Second thing!");
+}
+firstFunction(secondFunction);
+
+function makeCall(url, callback, callbackprint) {
+  $.ajax({
+    url: url,
+    success: function (response) {
+      console.log("The request succeeded!");
+      callback(response, callbackprint);
+    },
+    error: function (response) {
+      console.log("The request failed!");
+      callback(response);
+    },
+  });
+}
+function print(results, callback) {
+  console.log(results);
+  callback();
+}
+
+function print2() {
+  console.log("THE LAST JEDI");
+}
+makeCall("https://swapi.dev/api/people/1/", print, print2);
+
