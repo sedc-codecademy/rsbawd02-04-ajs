64ffa8207a5029c1072bf0ae91c8fd91d1f7569f
Aida Pirusic <pirusic.aida@nsoft.com>
2024-03-14 19:28:24 +0100

    Add one more example to materials

--- Class 10/materials/code/script.js
+++ Class 10/materials/code/script.js
@@ -1,177 +1,144 @@
-// function Person(id, name, gender) {
-//   this.id = id || "---";
+// // function Person(id, name, gender) {
+// //   this.id = id || "---";
+// //   this.name = name || "---";
+// //   this.gender = gender || "---";
+
+// //   this.displayDetails = () => {
+// //     console.log(`Id: ${this.id}`);
+// //     console.log(`Name: ${this.name}`);
+// //     console.log(`Gender: ${this.gender}`);
+// //   };
+// // }
+
+// // const person = new Person(1, "Aida Pirusic", "female");
+// // const student = Object.create(person);// copy
+// // student.academyName = 'Qinshift Academy';
+
+// // const employee = Object.create(person);
+// // employee.jobTtile = 'Frontend developer';
+
+// // console.log('person', person);
+// // console.log('student', student);
+// // console.log('student.name', student.name);
+// // console.log('student.academyName', student.academyName);
+
+// // console.log('person.__proto___', person.__proto__);
+// // console.log('student.__proto___', student.__proto__);
+
+// function Product(id, name, price) {
+//   this.id = Number(id) || 0;
 //   this.name = name || "---";
-//   this.gender = gender || "---";
+//   this.price = Number(price) || 0;
 
 //   this.displayDetails = () => {
 //     console.log(`Id: ${this.id}`);
 //     console.log(`Name: ${this.name}`);
-//     console.log(`Gender: ${this.gender}`);
-//   }
-// }
-
-// const person = new Person('1', 'Aida Pirusic', 'female');
-
-// const student = Object.create(person);
-// student.academyName = 'Qinshift Academy';
-// student.academyYear = 1;
-
-// const employee = Object.create(student);
-// employee.workPlace = 'Microsoft';
-// employee.jobTitle = 'Frontend developer';
-
-// // Prototype
-// console.log("person.__proto__", person.__proto__);
-// console.log("student.__proto__", student.__proto__);
-// console.log("employee.__proto__", employee.__proto__);
-// console.log("employee.__proto__.__proto__", employee.__proto__.__proto__);
-
-// // Adding methods on the prototype
-
-// student.__proto__.someTestFunction = () => console.log('This is test');
-// person.someTestFunction();// this works
-// student.someTestFunction();// this also works
-
-// // getPrototypeOf
-// console.log('student', Object.getPrototypeOf(student));
-// console.log('employee', Object.getPrototypeOf(employee));
-
-// function Person(id, name, gender) {
-//   this.id = id || "---";
-//   this.name = name || "---";
-//   this.gender = gender || "---";
-
-//   this.displayDetails = () => {
-//     console.log(`Id: ${this.id}`);
-//     console.log(`Name: ${this.name}`);
-//     console.log(`Gender: ${this.gender}`);
+//     console.log(`Price: ${this.price} $`);
 //   };
 // }
 
-// function Student(id, name, gender, academyName, academyYear) {
-//   //this.__proto__= Object.create(new Vehicle(id, name, batch, price));
-//   Object.setPrototypeOf(this, new Person(id, name, gender));
-
-//   this.academyName = academyName || "---";
-//   this.academyYear = Number(academyYear) || 0;
+// function ElectricalDevice(id, name, price, brand, model) {
+//   Object.setPrototypeOf(this, new Product(id, name, price));
+//   this.brand = brand || "---";
+//   this.model = model || "---";
 
 //   this.displayDetails = () => {
-//     // console.log(`Id: ${this.id}`);
-//     // console.log(`Name: ${this.name}`);
-//     // console.log(`Gender: ${this.gender}`);
 //     Object.getPrototypeOf(this).displayDetails();
-//     console.log(`Academy: ${this.academyName}`);
-//     console.log(`Academy year: ${this.academyYear}`);
+//     console.log(`Brand: ${this.brand}`);
+//     console.log(`Model: ${this.model}`);
 //   };
 // }
 
-// function Employee(
-//   id,
-//   name,
-//   gender,
-//   academyName,
-//   academyYear,
-//   companyName,
-//   jobTitle
-// ) {
+// function Laptop(id, name, price, brand, model, processor, isTouchScreen) {
 //   Object.setPrototypeOf(
 //     this,
-//     new Student(id, name, gender, academyName, academyYear)
+//     new ElectricalDevice(id, name, price, brand, model)
 //   );
 
-//   this.companyName = companyName || "---";
-//   this.jobTitle = jobTitle || "---";
+//   this.processor = processor || "---";
+//   this.isTouchScreen = isTouchScreen || false;
 
-//   this.displayDetails = () => {
-//     Object.getPrototypeOf(this).displayDetails(); // there will be called displayDetails from the Student template function
-//     console.log(`Company: ${this.companyName}`);
-//     console.log(`Job title: ${this.jobTitle}`);
+//   this.info = () => {
+//     Object.getPrototypeOf(this).displayDetails();
+//     console.log(`Processor: ${processor}`);
+//     console.log(isTouchScreen ? "Touch screen" : "Regular screen");
 //   };
 // }
 
-// const student = new Student("1", "Aida", "female", "Qinshift Academy", 1);
-// student.displayDetails();
-// console.log("\n");
-// const employee = new Employee(
-//   "1",
-//   "Aida",
-//   "female",
-//   "Qinshift Academy",
-//   1,
-//   "Microsoft",
-//   "Frontend developer"
+// function Clothes(id, name, price, size, material) {
+//   Object.setPrototypeOf(this, new Product(id, name, price));
+
+//   this.size = size || "---";
+//   this.material = material || "---";
+
+//   this.displayDetails = () => {
+//     Object.getPrototypeOf(this).displayDetails();
+//     console.log(`Size: ${size}`);
+//     console.log(`Material: ${material}`);
+//   };
+// }
+
+// console.log("PRODUCT:");
+// const product = new Product(1, "Dress", 35);
+// product.displayDetails();
+
+// console.log("\nELECTRICAL DEVICE:");
+
+// const elDevice = new ElectricalDevice(
+//   2,
+//   "Headphones",
+//   100,
+//   "Philips",
+//   "Fidelio L3"
 // );
-// employee.displayDetails();
+// elDevice.displayDetails();
 
-function Product(id, name, price) {
-  this.id = id || "---";
-  this.name = name || "---";
-  this.price = Number(price) || 0;
+// console.log("\nClothes");
+// const dress = new Clothes(3, "Little Black Dress", 35, "S", "cotton");
+// dress.displayDetails();
 
-  this.displayDetails = () => {
+// console.log("\nLaptop");
+// const laptop = new Laptop(4,"Laptop",1700,"HP","Notebook - 15-ac650tu","Intel Core i5",true);
+// console.log(laptop.name);
+// laptop.info();
+
+function Animal(id, species) {
+  this.id = Number(id) || 0;
+  this.species = species || "---";
+
+  this.info = () => {
     console.log(`Id: ${this.id}`);
-    console.log(`Name: ${this.name}`);
-    console.log(`Price: ${this.price} EUR`);
+    console.log(`Species: ${this.species}`);
   };
 }
 
-function ElectricalDevice(id, name, price, brand, model) {
-  Object.setPrototypeOf(this, new Product(id, name, price));
-  this.brand = brand || '---';
-  this.model = model || '---';
+function DomesticAnimal(id, species, name, age, vacinationDate) {
+  Object.setPrototypeOf(this, new Animal(id, species));
+
+  this.name = name || "---";
+  this.age = Number(age);
+  this.vacinationDate = vacinationDate || "";
 
   this.displayDetails = () => {
-    // console.log(`Id: ${this.id}`);
-    // console.log(`Name: ${this.name}`);
-    // console.log(`Price: ${this.price}`);
-    Object.getPrototypeOf(this).displayDetails();
-    console.log(`Brand: ${this.brand}`);
-    console.log(`Model: ${this.model}`);
-  }
+    Object.getPrototypeOf(this).info();
+
+    console.log(`Name: ${this.name}`);
+    console.log(`Age: ${this.age}`);
+    console.log(`Vacination date: ${this.vacinationDate}`);
+  };
 }
 
-function Clothes(id, name, price, size, material) {
-  Object.setPrototypeOf(this, new Product(id, name, price));
-  this.size = size || '---';
-  this.material = material || '---';
+function Cat(id, species, name, age, vacinationDate, breed) {
+  Object.setPrototypeOf(this, new DomesticAnimal(id, species, name, age, vacinationDate));
 
-  this.displayDetails = () => {
-    // console.log(`Id: ${this.id}`);
-    // console.log(`Name: ${this.name}`);
-    // console.log(`Price: ${this.price}`);
+  this.breed = breed || '---';
+
+  this.displayInformation = () => {
     Object.getPrototypeOf(this).displayDetails();
-    console.log(`Size: ${this.size}`);
-    console.log(`Material: ${this.material}`);
-  }
+
+    console.log(`Breed: ${this.breed}`);
+  };
 }
 
-function Laptop(id, name, price, brand, model, processor, isTouchScreen) {
-  Object.setPrototypeOf(this, new ElectricalDevice(id, name, price, brand, model));
-  this.processor = processor || '---';
-  this.isTouchScreen = isTouchScreen || false;
-
-  this.displayDetails = () => {
-    // console.log(`Id: ${this.id}`);
-    // console.log(`Name: ${this.name}`);
-    // console.log(`Price: ${this.price}`);
-    Object.getPrototypeOf(this).displayDetails();
-    console.log(`Processor: ${this.processor}`);
-    console.log(isTouchScreen ? 'Touch Screen' : 'Regular screen');
-  }
-}
-
-console.log('Product');
-const product = new Product(1, 'Some product', 1);
-product.displayDetails();
-
-console.log('\nElectricalDevice');
-const elDevice = new ElectricalDevice(2, 'Headphones', 100, 'Philips', 'Fidelio L3');
-elDevice.displayDetails();
-
-console.log('\nLaptop');
-const laptop = new Laptop(4, 'Laptop', 1700, 'HP', 'Notebook - 15-ac650tu', 'Intel Core i5', true);
-laptop.displayDetails();
-
-console.log('\nClothes');
-const dress = new Clothes(3, 'Little Black Dress', 35, 'S', 'cotton');
-dress.displayDetails();
+const cat = new Cat(1, 'cat', 'Kitty', 2, '14.3.2024.', 'Persian');
+cat.displayInformation();
