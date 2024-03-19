9700f98c0ff07062bf9cb9ce1574bdc7cdebb748
Aida Pirusic <pirusic.aida@nsoft.com>
2024-03-19 20:40:22 +0100

    Updated materials

--- Class 11/materials/script.js
+++ Class 11/materials/script.js
@@ -1,38 +1,42 @@
-// Inheritance with classes
-
 // class Person {
-//   constructor(id, name, gender) {
+//   constructor(id, name, gender, age) {
 //     this.id = Number(id) || 0;
 //     this.name = name || "---";
+//     this.lastName = lastName || "---";
+//     this.firstName = firstName || "---";
 //     this.gender = gender.toLowerCase() || "---";
+//     this.age = Number(age) || 0;
+//   }
+
+//   set name(personName) {
+//     if (!personName || personName.length < 2 || !isNaN(personName)) {
+//       console.error(
+//         "Person name is not valid. It needs to have at least 3 characters. And cannot be number."
+//       );
+//     } else {
+//       this._name = personName;
+//     }
 //   }
 
 //   get name() {
 //     return `The name is ${this._name}`;
 //   }
 
-//   set name(personName) {
-//     if (!personName || personName.length < 3) {
-//       console.error("Person name must have at least 3 characters!");
-//     } else {
-//       this._name = personName;
-//     }
-//   }
-
 //   displayDetails() {
 //     console.log(`Id: ${this.id}`);
 //     console.log(`Name: ${this.name}`);
 //     console.log(`Gender: ${this.gender}`);
+//     console.log(`Age: ${this.age}`);
 //   }
 
-//   static isFemale(person) {
-//     return person.gender.toLowerCase() === "female";
+//   static isFemale(obj) {
+//     return obj.gender.toLowerCase() === "female";
 //   }
 // }
 
 // class Student extends Person {
-//   constructor(id, name, gender, academyName, academyYear) {
-//     super(id, name, gender);
+//   constructor(id, name, gender, age, academyName, academyYear) {
+//     super(id, name, gender, age);
 
 //     this.academyName = academyName || "---";
 //     this.academyYear = Number(academyYear) || 1;
@@ -45,14 +49,15 @@
 //   }
 // }
 
-// class Employee extends Person {
-//   constructor(id, name, gender, company, jobTitle) {
-//     super(id, name, gender);
+// class Employee extends Person(Student) {
+//   constructor(id, name, gender, age, company, jobTitle) {
+//     super(id, name, gender, age);
 
 //     this.company = company || "---";
 //     this.jobTitle = jobTitle || "---";
 //   }
 
+//   // override function
 //   displayDetails() {
 //     super.displayDetails();
 //     console.log(`Company: ${this.company}`);
@@ -60,61 +65,60 @@
 //   }
 // }
 
-// const person1 = new Person(1, "Aida P", "female");
-// const person2 = new Person(2, "Milica D", "female");
+// const person1 = new Person(1, "Aida P", "female", 22);
+// const person2 = new Person(2, "Djordje L", "male", 20);
+
+// // person1.name = 'F';
 
 // person1.displayDetails();
 // console.log("\n");
 // person2.displayDetails();
-// console.log("\n");
 
-// const st1 = new Student(3, "Djordje L", "male", "Qinshift", 2);
-// const st2 = new Student(4, "Goran R", "male", "Qinshift", 1);
-
+// const st1 = new Student(3, "Milica D", "female", 20, "Qinshift academy", 1);
+// console.log("displayDetails:");
+// st1.displayDetails();
+// console.log("\ndisplayInfo:");
 // st1.displayInfo();
-// console.log("\n");
-// st2.displayInfo();
-// console.log("\n");
+// console.log("\n:");
 
-// // Math.pow(3,2);
-// console.log(Person.isFemale(person1));
-// console.log(Person.isFemale(st1));
-
-// st1.name = "A";
-// console.log(st1.name); // still Djordje L
-
-// const em1 = new Employee(
-//   5,
-//   "John Doe",
-//   "male",
+// const empl1 = new Employee(
+//   4,
+//   "Tamara Z",
+//   "female",
+//   20,
 //   "Microsoft",
 //   "Frontend developer"
 // );
-// em1.displayDetails();
+// empl1.displayDetails();
 
-// const whoAmI = (obj) => {
+// // STATIC METHOD
+// Math.pow(3, 2);
+// // console.log(Person.isFemale(person1));
+// // console.log(Person.isFemale(person2));
+
+// console.log(empl1 instanceof Person); //true
+// console.log(empl1 instanceof Employee);
+// console.log(empl1 instanceof Student); //false
+
+// function whoAmI(obj) {
 //   if (obj instanceof Student) {
-//     console.log(`I'm STUDENT`);
+//     console.log("IT IS A STUDENT");
 //   } else if (obj instanceof Employee) {
-//     console.log(`I'm EMPLOYEE`);
+//     console.log("IT IS AN EMPLOYEE");
 //   } else if (obj instanceof Person) {
-//     console.log(`I'm just a person`);
+//     console.log("IT IS A PERSON");
 //   } else {
-//     console.log("ERROR");
+//     console.log("Error");
 //   }
-// };
-
-// console.log(person1 instanceof Person);
-// console.log(st1 instanceof Student);
-// console.log(st1 instanceof Employee);
-// console.log(em1 instanceof Employee);
+// }
 
 // whoAmI(person1);
 // whoAmI(st1);
-// whoAmI(em1);
-// whoAmI({ test: 123 });
+// whoAmI(empl1);
+// whoAmI({ test: 1234 });
 
 // EXAMPLE 1
+
 class Product {
   constructor(id, name, price) {
     this.id = Number(id) || 0;
@@ -125,7 +129,7 @@ class Product {
   displayDetails() {
     console.log(`Id: ${this.id}`);
     console.log(`Name: ${this.name}`);
-    console.log(`Price: ${this.price} $`);
+    console.log(`Price: ${this.price} EUR`);
   }
 }
 
@@ -147,11 +151,12 @@ class ElectricalDevice extends Product {
 class Laptop extends ElectricalDevice {
   constructor(id, name, price, brand, model, processor, isTouchScreen) {
     super(id, name, price, brand, model);
+
     this.processor = processor || "---";
     this.isTouchScreen = isTouchScreen || false;
   }
 
-  info() {
+  displayDetails() {
     super.displayDetails();
     console.log(`Processor: ${this.processor}`);
     console.log(this.isTouchScreen ? "Touch screen" : "Regular screen");
@@ -161,12 +166,28 @@ class Laptop extends ElectricalDevice {
 class Clothes extends Product {
   constructor(id, name, price, size, material) {
     super(id, name, price);
-    this.size = size || "---";
+    this.size = size || "S";
     this.material = material || "---";
   }
 
-  displayDetails() {
-    super.displayDetails();
+  set size(newSize) {
+    const uppercasedSize = newSize.toUpperCase(); //s => S
+    const validSizes = ["XXS", "XS", "S", "M", "L", "XL", "XXL"];
+
+    if (validSizes.includes(uppercasedSize)) {
+      this._size = uppercasedSize;
+    } else {
+      console.error("Size is not valid!");
+      this._size = "S";
+    }
+  }
+
+  get size() {
+    return this._size.toUpperCase();
+  }
+
+  displayInfo() {
+    this.displayDetails();
     console.log(`Size: ${this.size}`);
     console.log(`Material: ${this.material}`);
   }
@@ -177,7 +198,6 @@ const product = new Product(1, "Dress", 35);
 product.displayDetails();
 
 console.log("\nELECTRICAL DEVICE:");
-
 const elDevice = new ElectricalDevice(
   2,
   "Headphones",
@@ -186,10 +206,6 @@ const elDevice = new ElectricalDevice(
   "Fidelio L3"
 );
 elDevice.displayDetails();
-
-console.log("\nClothes");
-const dress = new Clothes(3, "Little Black Dress", 35, "S", "cotton");
-dress.displayDetails();
 
 console.log("\nLaptop");
 const laptop = new Laptop(
@@ -202,4 +218,31 @@ const laptop = new Laptop(
   true
 );
 console.log(laptop.name);
-laptop.info();
+laptop.displayDetails();
+
+console.log("\nClothes");
+const dress = new Clothes(3, "Little Black Dress", 35, "j", "cotton");
+dress.displayInfo();
+
+
+class Person {
+  constructor(id, name, gender, age) {
+    this.id = Number(id) || 0;
+    this.name = name || "---";
+    this.gender = gender.toLowerCase() || "---";
+    this.age = Number(age) || 0;
+  }
+
+  getFullName(person) {
+    console.log(this.name);
+    console.log(person.name);
+  }
+
+}
+
+const p1 = new Person(1, 'AIda', 'female', 20);
+const p2 = new Person(1, 'Djordje', 'male', 20);
+
+p1.getFullName(p2);
+
+
